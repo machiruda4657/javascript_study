b066b3443365a40df2fa60273388b1c2d99b1f20
町田祐太 <Yutaman@yuutakun-no-MacBook-Air.local>
2017-06-27 13:56:45 +0900

    add all

---
+++ main.js
@@ -0,0 +1,39 @@
+function print(name, year, seibetu) {
+  console.log(name + "の年齢は" + year + "歳です。性別は" + seibetu + "です。"
+  )
+}
+
+print("ゆうた", 28, "男")
+print("かおり", 33, "女")
+print("みく", 19, "女")
+print("もじゃ", 34, "男")
+
+
+
+console.log("herro world!!")
+
+var x = 10;
+console.log(x+9)
+console.log(x*3)
+console.log(x/2)
+
+function print(kion,ondo){
+  console.log(kion  + "℃ "+ ondo +"です"
+  )
+}
+
+print(,30,"温度は")
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
